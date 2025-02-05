a7871549772df31f092053d9dbb7178bc0da82a0
Kaaydong <48571593+Kaaydong@users.noreply.github.com>
2025-02-04 20:09:52 -0800

    Site is now deployable on WSGI
    Also some changes to command list

--- company_site/company_site/settings.py
+++ company_site/company_site/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-qu5hj$#u4p5c9fy8=v^ccs9383@)%ghj=l@^c94x+nvxguy9jl
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -116,6 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "static_collection"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
--- company_site/company_site/wsgi.py
+++ company_site/company_site/wsgi.py
@@ -7,9 +7,16 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
+from pathlib import Path
 
 from django.core.wsgi import get_wsgi_application
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+sys.path.append(BASE_DIR)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'company_site.settings'
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'company_site.settings')
 
