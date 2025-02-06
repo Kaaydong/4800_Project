706cb76b6a1972be8468b384c45153d7ab7a5050
Kaaydong <48571593+Kaaydong@users.noreply.github.com>
2025-02-06 01:22:28 -0800

    company_site mostly done
    Everything is pretty much done
    - one thing is that CSRF detection is tripping when trying to access admin page through AWS, so we must fix that

--- company_site/home_app/views.py
+++ company_site/home_app/views.py
@@ -13,4 +13,34 @@ def crew(request):
 
 def meeting_logs(request):
     all_logs = MeetingLog.objects.all()
-    return render(request, 'home_app/meeting_logs.html', {'log_list': all_logs})
+    all_logs = reversed(all_logs)
+
+    all_present_lists = []
+    all_absent_lists = []
+    for log in reversed(MeetingLog.objects.all()):
+        present_list = ""
+        absent_list = ""
+
+        if log.arnav_present: present_list += "|  Arnav Rahman  |"
+        else: absent_list += "|  Arnav Rahman  |"
+
+        if log.kayden_present: present_list += "|  Kayden Hung  |"
+        else: absent_list += "|  Kayden Hung  |"
+
+        if log.ian_present: present_list += "|  Ian Wong  |"
+        else: absent_list += "|  Ian Wong  |"
+
+        if log.keon_present: present_list += "|  Keon Der  |"
+        else: absent_list += "|  Keon Der  |"
+
+        if log.bao_present: present_list += "|  Bao Tran  |"
+        else: absent_list += "|  Bao Tran  |"
+
+        if len(absent_list) == 0: absent_list += "None"
+
+        all_present_lists.append(present_list)
+        all_absent_lists.append(absent_list)
+
+    combined_list = list(zip(all_logs, all_present_lists, all_absent_lists))
+
+    return render(request, 'home_app/meeting_logs.html', {'combined_log_list': combined_list})
